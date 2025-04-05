2ffa7d9ecaec6f70287bdc90f1f773d356af6450
DavitiChkhikvadze <data.poshki2020@gmail.com>
2025-04-05 12:57:27 +0400

    commit

---
+++ day 119/homework/homework119.py
@@ -0,0 +1,59 @@
+#ინტერვიუსთვის ჩემი მოგონილი კითხვები და ამოცანები:
+
+#I. ადვილი კითხვები (3)
+#1 - რა არის ცვლადი და როგორ ხდება მისი გამოცხადება JavaScript-სა და Python-ში?
+#2️ - რა განსხვავებაა == და === ოპერატორებს შორის JavaScript-ში? Python-ში როგორ ხდება მსგავსი შედარება?
+#3️ - რა შედეგს დააბრუნებს შემდეგი კოდი?
+#Python:
+
+print(2 * "3" + 3 * "2")
+
+#JavaScript:
+
+#console.log(2 * "3" + 3 * "2");
+
+#II. საშუალო დონის ამოცანები (2)
+#4️ - ფუნქცია "პალინდრომი"
+#დაწერეთ ფუნქცია, რომელიც შეამოწმებს, არის თუ არა მოცემული ტექსტი პალინდრომი (იგივე წინიდან და უკნიდან).
+#Python:
+
+#def is_palindrome(s):
+    #კოდიაქ
+#print(is_palindrome("racecar"))
+
+#JavaScript:
+
+#function isPalindrome(s) {
+    #აქ კოდი
+#}
+#console.log(isPalindrome("racecar")); // true
+
+#5️ - მონაცემთა ფილტრაცია
+#დაწერეთ ფუნქცია, რომელიც იღებს რიცხვების სიას/მასივს და აბრუნებს მხოლოდ ლუწ რიცხვებს.
+#Python:
+def filter_even(numbers):
+    pass  # აქ კოდი
+print(filter_even([1, 2, 3, 4, 5, 6]))  # [2, 4, 6]
+
+#JavaScript:
+
+#function filterEven(numbers) {
+    #აქ კოდი
+#}
+#console.log(filterEven([1, 2, 3, 4, 5, 6])); // [2, 4, 6]
+
+#III. რთული ამოცანა (1)
+#6️ - ანაგრამების შემოწმება
+#დაწერეთ ფუნქცია, რომელიც შეამოწმებს, არის თუ არა ორი სიტყვა ანაგრამი (შეიცავს იგივე ასოებს, მაგრამ სხვანაირად განლაგებულს).
+#Python:
+
+def is_anagram(s1, s2):
+    pass  # აქ კოდი
+print(is_anagram("listen", "silent"))  # True
+
+#JavaScript:
+
+#function isAnagram(s1, s2) {
+    #აქ დაამატეთ თქვენი კოდი
+#}
+#console.log(isAnagram("listen", "silent")); // true
